53d4adf7822c18a2b70456f4b62bb1d22bd5df2d
Grimowsky <43293271+Grimowsky@users.noreply.github.com>
2024-03-07 18:08:10 +0100

    Task/setup client boilerplate (#6)
    * add readme
    
    * setup vite local congit
    
    * add polling to vite config
    
    * cleanup
    
    * setup tailwind and tailwind theme
    
    * sync package lock
    
    * fix linting

--- client/src/utils/utilTest.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { add } from './utilTest';
-
-describe('Util test', () => {
-  it('Should add 2 numbers', () => {
-    expect(add(2, 2)).eq(4);
-  });
-});
