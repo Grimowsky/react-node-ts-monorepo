a4b20a3986314e34ed8144b7d0c4109e9b5b6e65
Grimm <rafalmirkowski@gmail.com>
2024-03-07 17:21:47 +0100

    cleanup

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,12 +1,12 @@
+import React from "react";
 import { routes } from './routes.schema';
 import { Route, Routes } from 'react-router-dom';
-import { NavigateTest } from '../components/NavigateTest/NavigateTest';
+
 
 export const AppRoutes = (): React.ReactElement => {
   return (
     <Routes>
-      <Route path={routes.BASE_ROUTE} element={<>elegancko dziala kierowniku</>} />
-      <Route path={'/hello'} element={<NavigateTest />} />
+      <Route path={routes.BASE_ROUTE} element={<>hello world</>} />
     </Routes>
   );
 };
